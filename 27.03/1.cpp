b5201f25a7a374835f2b4cfd4543d7a0d9c937c6
gvheisler <gvheisler@inf.ufsm.br>
2024-03-27 09:48:41 -0300

    aprendendo C kkkkk

---
+++ 27.03/1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int n;
+    double x;
+
+    n = 10.1;
+    x = 22.4;
+
+    cout << "n = " << n << endl;
+    cout << "x = " << x << endl;
+
+}
